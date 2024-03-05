7eeb2c95fa9fe1e8ba5710ef9e07e1d95ba741c6
Elijah Thode <fleezuschrist@Elijahs-MacBook-Pro.local>
2024-03-04 23:35:25 -0700

    Added indexx, css style and main Java for page

---
+++ main.js
@@ -0,0 +1,23 @@
+/*  Psuedocode
+1. Define the variables needed for this assignment.
+2. Assign the variables with the necessary arithmetic operators.
+Create the dialog box using the 'alert' method.
+Add comments for explanations of code where necessary.
+*/
+
+// Declared the variables needed for this exercise
+
+const combo1 = 5 + 5;
+const combo2 = 4*10;
+const combo3 = 239 % 2;
+const messageStr = "You have received this message because you have been chosen to open an important vault. Her is the secret combination:"
+
+// Used'alert' method for the dialog box pop-up.
+
+alert (messagstr + combo1 + dashStr +combo2 + dashStr + combo3);
+
+let userEnter = prompt("Please enter first combination number");
+let userEnter2 = prompt("Please enter second combination number");
+let userEnter3 = prompt("Please enter third combination number");
+
+alert ( messagStr + userEnter + dashStr +userEnter2 + dashstr + userEnter3 ) 
